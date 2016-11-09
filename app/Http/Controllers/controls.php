402d2100efcbf7b953e2d4016d5c2a5db19487ea
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-11-09 11:11:22 -0400

    Subiendo la pagina de 404.blade.php

--- app/Http/Controllers/controls.php
+++ app/Http/Controllers/controls.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Cla;
+use App\ClaPersona;
 
 class controls extends Controller
 {
+    function __construct()
+    {
+        set_time_limit(60*8); //60 seconds = 1 minute
+//        ini_set('memory_limit', '128M');
+//        Carbon::setLocale('es');
+    }
     public function index(){
-        return "Comenzamos";
+        $fileName = "historial.xlsx";
+        $fileName = "pacientesa.xlsx";
+        \Excel::load($fileName, function($reader){
+//            return $reader;
+            echo "<pre>";
+            $reader->sheet('pacientes',function($sheet)
+            {
+//                return $sheet;
+//                    echo "X";
+                foreach ($sheet->toArray() as  $key=>$row)
+                {
+                    print_r($row);
+                    list($code,$fullName,$phone,$cellphone,$doctorName,$date) = $row;
+                    $persona = [
+                        ''=>'',
+                    ];
+                    Cla::create();
+                }
+            });
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/make/job',['as'=>'excel','uses'=>'controls@index']);
